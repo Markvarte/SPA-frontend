390bddbe241c7df2713103e95e9d56b09264fe58
Natalija Markvarte <natalija.markvarte@bbit.tech>
2020-07-14 17:32:42 +0300

    Changed serverUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,7 +14,7 @@ export const environment = {
   production: false,
   hmr: true,
   version: env.npm_package_version + '-dev',
-  serverUrl: '/api',
+  serverUrl: 'http://localhost:49928/api', // for local, api url
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US']
 };
