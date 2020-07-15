afb0eddf00cc3c3b62dd3c28150aa29bfbc0ef71
Natalija Markvarte <natalija.markvarte@bbit.tech>
2020-07-15 13:34:42 +0300

    create main routing for displaying list

--- src/app/house/house-routing.module.ts
+++ src/app/house/house-routing.module.ts
@@ -3,10 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { extract } from '@app/i18n';
 import { HouseListComponent } from './house-list/house-list.component';
+import { Shell } from '@app/shell/shell.service';
 
 const routes: Routes = [
-  // Module is lazy loaded, see app-routing.module.ts
-  { path: '', component: HouseListComponent, data: { title: extract('houses') } }
+  Shell.childRoutes([
+    { path: '', redirectTo: '/houses', pathMatch: 'full', data: { title: extract('Houses') } },
+    // path: '' by default display house list
+    { path: 'houses', component: HouseListComponent, data: { title: extract('Houses') } } // route to house list
+  ])
 ];
 
 @NgModule({
