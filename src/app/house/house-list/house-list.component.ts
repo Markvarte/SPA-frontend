8d5d4d7717a1c43669f771c00fcbeab445ec1afe
Natalija Markvarte <natalija.markvarte@bbit.tech>
2020-07-15 15:57:48 +0300

    display house list

--- src/app/house/house-list/house-list.component.ts
+++ src/app/house/house-list/house-list.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { House, DefaultHouse } from '../house-interface/default-house';
+import { HouseServiceService } from '../house-service.service';
+import * as _ from 'lodash';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-house-list',
@@ -6,10 +11,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./house-list.component.css']
 })
 export class HouseListComponent implements OnInit {
+  public houses: Array<House>; // Array of Houses, which will be displayed
+  public defaultHouse: House; // for initializing default values (null and empty strings)
+  constructor(
+    private houseService: HouseServiceService, // for sending requests to server
+  ) {
+    this.defaultHouse = new DefaultHouse(); // initializing default values (null and empty strings)
+  }
 
-  constructor() { }
-
+  public deleteHouse(houseId: number) {
+    const deleteIndex = _.findIndex(this.houses, { id: houseId }); // я забыла как без lodash и не могу придумать
+    this.houseService.remove(houseId).subscribe( // delete house from server and on subscribe return it back
+      () => this.houses.splice(deleteIndex, 1) // delete house from array
+    );
+  }
   ngOnInit() {
+    this.houseService.get().subscribe((data: Array<House>) => this.houses = data); // get house list from server
+    // and put it into houses array to display
   }
 
 }
