60575e2d1bc3608611737b55079b55bd63922a98
Natalija Markvarte <natalija.markvarte@bbit.tech>
2020-07-16 15:57:59 +0300

    create flat module

---
+++ src/app/flat/flat.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlatListComponent } from './flat-list/flat-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    FlatListComponent]
+})
+export class FlatModule { }
