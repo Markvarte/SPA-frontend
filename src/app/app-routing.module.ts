c319c7f81f71ac5ba0d59ef7c80d7f3fe1dd913d
Natalija Markvarte <natalija.markvarte@bbit.tech>
2020-07-21 12:12:10 +0300

    delete about component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,6 @@ import { Shell } from '@app/shell/shell.service';
 
 const routes: Routes = [
   Shell.childRoutes([
-    { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) }, 
-    // TODO: delete path: 'about', i guess
     { path: 'flats', loadChildren: () => import('./flat/flat.module').then(m => m.FlatModule) },
     { path: 'tenants', loadChildren: () => import('./tenant/tenant.module').then(m => m.TenantModule) },
   ]),
